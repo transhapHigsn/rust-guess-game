9148943e4174a4174dd54b25194183dd6031f7f6
transhapHigsn <fet.prashantsingh@gmail.com>
2020-06-29 15:08:13 +0530

    added scoring system to game

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,11 @@ fn main() {
     println!("Guess a number!");
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
+    let mut score: f64 = 100.0;
+    const ALLOWED_ATTEMPTS: u32 = 3;
+
+    let mut attempts: u32 = 0;
+
     loop {
         println!("Please input your guess.");
 
@@ -19,12 +24,24 @@ fn main() {
             Err(_) => continue,
         };
         println!("You guessed {}", guess);
+
+        attempts += 1;
         
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => {
+                println!("Too small!");
+                if attempts > ALLOWED_ATTEMPTS {
+                    score = 0.9 * score;
+                }
+            },
+            Ordering::Greater => {
+                println!("Too big!");
+                if attempts > ALLOWED_ATTEMPTS {
+                    score = 0.9 * score;
+                } 
+            },
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! Your score is: {:.1$}", score, 2);
                 break;
             }
         }
